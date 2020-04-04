00832952cb95caf6a7aef6b90769db36249fd9d8
Ivan Shershnev <ivankrut856@yandex.ru>
2020-04-04 20:39:09 +0300

    add static files (this time for sure)

---
+++ build/precache-manifest.6724fbd3696cb929a99f56907333992f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12d424f83f48892732123867081cc4c0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5958e203e2def66ad4a5",
+    "url": "./static/js/2.dc6dfc4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.dc6dfc4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "577972cef240ac6b6e20",
+    "url": "./static/js/main.c3734252.chunk.js"
+  },
+  {
+    "revision": "f7294d2be2a7cdf96c65",
+    "url": "./static/js/runtime-main.3fe82ad8.js"
+  }
+]);
